c91fcd0912f99da766ae4d6aef09873877c7c563
Jepoy <jeffreymijares123@gmail.com>
2022-12-08 21:01:50 -0500

    xml

---
+++ app/src/main/java/rygatoni/github/io/chicago_pizza.java
@@ -0,0 +1,14 @@
+package rygatoni.github.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chicago_pizza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chicago_pizza);
+    }
+}
